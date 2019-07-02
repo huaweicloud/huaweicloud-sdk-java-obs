0279af4f39dcd9e7a6710110af79e75b5a57e935
x00403408 <44666668+x00403408@users.noreply.github.com>
2019-07-02 18:53:52 +0800

    Update ProgressInputStream.java

--- app/src/main/java/com/obs/services/internal/io/ProgressInputStream.java
+++ app/src/main/java/com/obs/services/internal/io/ProgressInputStream.java
@@ -1,4 +1,6 @@
 /**
+ * Copyright 2013-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
  * Copyright 2019 Huawei Technologies Co.,Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
  * this file except in compliance with the License.  You may obtain a copy of the
